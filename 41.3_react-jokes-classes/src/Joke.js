85548e66f3c5af56e9dce4c83906232bd8e14727
Joyce <hellojoycewong@gmail.com>
2021-07-16 13:36:11 -0700

    Add class base components to Joke.js

---
+++ 41.3_react-jokes-classes/src/Joke.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+import "./Joke.css";
+
+
+class Joke extends React.Component{
+  render(){
+    return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={this.props.upVote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
+
+        <button onClick={this.props.downVote}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+
+        {this.props.votes}
+      </div>
+
+      <div className="Joke-text">{this.props.text}</div>
+    </div>)
+  }
+}
+
+
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
+
+//   return (
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
+
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
+
+//         {votes}
+//       </div>
+
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
+
+export default Joke;
